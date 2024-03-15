5472dcb596b71aeae7fa302ee604f53a7d4fad15
heesane <tees3359@gmail.com>
2024-03-15 20:38:40 +0900

    chore: Spring Boot Setting

---
+++ backend/src/main/java/fitapet/backend/fit_a_pet/FitAPetApplication.java
@@ -0,0 +1,13 @@
+package fitapet.backend.fit_a_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitAPetApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FitAPetApplication.class, args);
+    }
+
+}
