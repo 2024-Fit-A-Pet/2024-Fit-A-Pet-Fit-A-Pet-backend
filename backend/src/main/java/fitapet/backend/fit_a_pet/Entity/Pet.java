98bf19f50768bc677f1dd38bb65b6f11be1f68f7
heesane <tees3359@gmail.com>
2024-04-03 20:13:32 +0900

    [#12]feat: add Pet Entity

---
+++ backend/src/main/java/fitapet/backend/fit_a_pet/Entity/Pet.java
@@ -0,0 +1,46 @@
+package fitapet.backend.fit_a_pet.Entity;
+
+import fitapet.backend.fit_a_pet.common.CoreEntity;
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Table(name="pet")
+@Entity
+public class Pet extends CoreEntity {
+
+    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @Column(name="pet_id")
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @Column(name="name")
+    private String name;
+
+    @Column(name="age")
+    private int age;
+
+    @Column(name="species")
+    private String species;
+
+    @Column(name="gender")
+    private String gender;
+
+    @Column(name="weight")
+    private double weight;
+
+    @Column(name="started_date")
+    private LocalDateTime started_date;
+
+    @Column(name="feed")
+    private String feed;
+
+    @Column(name="sore_sport")
+    private String sore_sport;
+
+    @Column(name="profile_url")
+    private String profile_url;
+}
