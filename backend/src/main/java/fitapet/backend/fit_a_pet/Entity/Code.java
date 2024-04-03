524950287fb96640e0d621516e2ea537f835b218
heesane <tees3359@gmail.com>
2024-04-03 20:11:09 +0900

    [#12]feat: add FK into code Entity

--- backend/src/main/java/fitapet/backend/fit_a_pet/Entity/Code.java
+++ backend/src/main/java/fitapet/backend/fit_a_pet/Entity/Code.java
@@ -1,23 +1,21 @@
 package fitapet.backend.fit_a_pet.Entity;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Size;
 import lombok.Data;
-import org.hibernate.annotations.Formula;
 
 @Entity
 @Table(name="code")
 @Data
 public class Code {
     @Id
-    @Column(name="id")
+    @Column(name="code_id")
     @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name="pet_id")
-    private Long pet_id;
+    @Column(name="device_code",length = 32, nullable = false, unique = true)
+    private String device_code;
 
-    @Column(name="code",length = 32, nullable = false, unique = true)
-    private String code;
-
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name="pet_id")
+    private Pet pet;
 }
