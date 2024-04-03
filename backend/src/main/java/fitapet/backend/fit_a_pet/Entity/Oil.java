872b6dfa0261557033a6c34e821514177fdadfc8
heesane <tees3359@gmail.com>
2024-04-03 14:39:55 +0900

    [#12]fix: import core entity

--- backend/src/main/java/fitapet/backend/fit_a_pet/Entity/Oil.java
+++ backend/src/main/java/fitapet/backend/fit_a_pet/Entity/Oil.java
@@ -1,13 +1,14 @@
 package fitapet.backend.fit_a_pet.Entity;
 
 
+import fitapet.backend.fit_a_pet.common.CoreEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 
 @Table(name="oil")
 @Entity
-public class Oil extends CoreEntity{
+public class Oil extends CoreEntity {
 
     @Column(name="oil_name")
     private String oilName;
