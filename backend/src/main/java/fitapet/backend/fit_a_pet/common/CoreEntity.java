08ff5dbca00a9fcf82297f2e438d90febbb94ca8
heesane <tees3359@gmail.com>
2024-04-03 14:01:24 +0900

    [#12]feat: Core Entity

---
+++ backend/src/main/java/fitapet/backend/fit_a_pet/common/CoreEntity.java
@@ -0,0 +1,32 @@
+package fitapet.backend.fit_a_pet.common;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Data
+@EntityListeners({AuditingEntityListener.class})
+public class CoreEntity {
+
+    @Id
+    @Column(name="id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+    @PreRemove
+    public void preRemove() {
+        this.deletedAt = LocalDateTime.now();
+    }
+}
