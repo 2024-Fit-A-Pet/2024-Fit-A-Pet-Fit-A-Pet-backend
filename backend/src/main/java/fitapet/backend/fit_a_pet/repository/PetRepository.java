efa5a8292bc631cbd03e1da63452cfa4af32f357
limjihoon99 <bo9701@naver.com>
2024-04-08 01:41:06 +0900

    [#11]fix: PetRepository 수정

--- backend/src/main/java/fitapet/backend/fit_a_pet/repository/PetRepository.java
+++ backend/src/main/java/fitapet/backend/fit_a_pet/repository/PetRepository.java
@@ -1,4 +1,8 @@
 package fitapet.backend.fit_a_pet.repository;
 
-public interface PetRepository {
+import fitapet.backend.fit_a_pet.Entity.Pet;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PetRepository extends JpaRepository<Pet, Long> {
+
 }
