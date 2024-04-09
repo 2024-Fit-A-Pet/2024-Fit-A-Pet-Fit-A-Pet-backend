600e71fbbd77f01cd196435c40574ccfd8330ff8
heesane <tees3359@gmail.com>
2024-04-09 18:23:37 +0900

    [#18]fix: 불필요한 import 삭제

--- backend/src/main/java/fitapet/backend/fit_a_pet/FitAPetApplication.java
+++ backend/src/main/java/fitapet/backend/fit_a_pet/FitAPetApplication.java
@@ -1,9 +1,7 @@
 package fitapet.backend.fit_a_pet;
 
-import fitapet.backend.fit_a_pet.util.YamlLoadFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 public class FitAPetApplication {
