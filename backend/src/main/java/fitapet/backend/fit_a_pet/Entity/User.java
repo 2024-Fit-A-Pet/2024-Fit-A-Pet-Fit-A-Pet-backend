26799b9d0b05f26c28d413532993b75f126dbbd8
limjihoon99 <bo9701@naver.com>
2024-04-09 17:57:32 +0900

    [#16]fix: User Entity 수정

--- backend/src/main/java/fitapet/backend/fit_a_pet/Entity/User.java
+++ backend/src/main/java/fitapet/backend/fit_a_pet/Entity/User.java
@@ -8,27 +8,43 @@ import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Entity
-@Table(name="users")
+@Table(name = "users")
 @EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor
 @Setter
 @Getter
 @ToString
-public class User extends CoreEntity{
+public class User extends CoreEntity {
 
-    @Id @GeneratedValue(strategy= GenerationType.IDENTITY)
-    @Column(name="user_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long id;
 
-    @Column(name="name",nullable = false)
-    @Size(min=4, max=15)
-    private String name;
-
     @Email
-    @Size(min=4, max=50)
-    @Column(name="email",nullable = false)
+    @Size(min = 4, max = 50)
+    @Column(name = "email", nullable = false, unique = true)
     private String email;
 
-    @Column(name="password",nullable = false)
+    @Column(name = "name", nullable = false)
+    @Size(min = 4, max = 15)
+    private String name;
+
+    @Column(name = "password", nullable = false)
     private String password;
+
+    @Column(name = "phone_num", nullable = false)
+    private String phoneNum;
+
+    @Column(name = "zip_code", nullable = false)
+    private String zipCode;
+
+    @Column(name = "address", nullable = false)
+    private String address;
+
+    @Column(name = "email_verified", nullable = false)
+    private Boolean emailVerified = false;
+
+    @Column(name = "phone_num_verified", nullable = false)
+    private Boolean phoneNumVerified = false;
 }
