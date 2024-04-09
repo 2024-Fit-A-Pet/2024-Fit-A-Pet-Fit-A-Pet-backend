36a7fcd7fa64f271da8f0b0cb2ab35515e8fc6c6
heesane <tees3359@gmail.com>
2024-04-09 16:30:12 +0900

    [#18]feat: add Annotation for PropertySource

--- backend/src/main/java/fitapet/backend/fit_a_pet/FitAPetApplication.java
+++ backend/src/main/java/fitapet/backend/fit_a_pet/FitAPetApplication.java
@@ -1,9 +1,12 @@
 package fitapet.backend.fit_a_pet;
 
+import fitapet.backend.fit_a_pet.util.YamlLoadFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource(value = {"classpath:env.yml"}, factory= YamlLoadFactory.class)
 public class FitAPetApplication {
 
     public static void main(String[] args) {
