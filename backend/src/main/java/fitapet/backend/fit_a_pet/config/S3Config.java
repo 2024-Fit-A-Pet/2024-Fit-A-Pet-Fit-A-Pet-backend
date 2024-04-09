cb786b2abbb2169badb52fd893a5ff6bc3920487
heesane <tees3359@gmail.com>
2024-04-09 18:11:07 +0900

    [#18]feat: add S3 Config

---
+++ backend/src/main/java/fitapet/backend/fit_a_pet/config/S3Config.java
@@ -0,0 +1,36 @@
+package fitapet.backend.fit_a_pet.config;
+
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import fitapet.backend.fit_a_pet.util.YamlLoadFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+public class S3Config {
+
+    @Value("${AWS.SECRET_KEY}")
+    private String secretKey;
+
+    @Value("${AWS.ACCESS_KEY}")
+    private String accessKey;
+
+    @Value("${AWS.REGION}")
+    private String region;
+
+    @Bean
+    public AmazonS3 amazonS3Client() {
+        BasicAWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
+        return  AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .enablePathStyleAccess()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
