34e9ff2917dd006f2d50d331c0b76ee1f9aafa05
heesane <tees3359@gmail.com>
2024-04-23 03:47:58 +0900

    ㅁㄴㅇ

--- backend/src/main/java/fitapet/backend/fit_a_pet/Entity/Gender.java
+++ backend/src/main/java/fitapet/backend/fit_a_pet/Entity/Gender.java
@@ -1,4 +1,13 @@
 package fitapet.backend.fit_a_pet.Entity;
 
 public enum Gender {
+    // 수컷
+    // 암컷
+    // 중성화된 수컷
+    // 중성화된 암컷
+
+    MALE,
+    FEMALE,
+    NEUTER_MALE,
+    NEUTER_FEMALE
 }
